4d6046ed0fe69dbe06b42a6558b87f83f7fba734
Gianluca Petrillo <petrillo@fnal.gov>
2018-01-31 18:25:49 -0600

    Added drift output to DetectorPropertiesStandard test

--- lardataalg/test/DetectorInfo/DetectorPropertiesStandard_test.cc
+++ lardataalg/test/DetectorInfo/DetectorPropertiesStandard_test.cc
@@ -6,13 +6,14 @@
  */
 
 // LArSoft libraries
-#include "test/Geometry/geometry_unit_test_base.h"
-#include "larcorealg/Geometry/ChannelMapStandardAlg.h"
-#include "larcorealg/Geometry/GeometryCore.h"
 #include "lardata/DetectorInfo/LArPropertiesStandardTestHelpers.h"
 #include "lardata/DetectorInfo/DetectorClocksStandardTestHelpers.h"
 #include "lardata/DetectorInfo/DetectorPropertiesStandard.h"
 #include "lardata/DetectorInfo/DetectorPropertiesStandardTestHelpers.h"
+#include "larcorealg/Geometry/ChannelMapStandardAlg.h"
+#include "larcorealg/Geometry/GeometryCore.h"
+#include "larcorealg/CoreUtils/RealComparisons.h"
+#include "test/Geometry/geometry_unit_test_base.h"
 
 // C/C++ standard libraries
 #include <iomanip>
@@ -114,21 +115,17 @@ int main(int argc, char const** argv) {
   // 3. then we run it!
   auto const& geom = *(TestEnv.Provider<geo::GeometryCore>());
   auto const& detp = *(TestEnv.Provider<detinfo::DetectorProperties>());
+  
+  auto const driftVelocity = detp.DriftVelocity();
+  auto const TDCtick = detp.SamplingRate();
+
   mf::LogVerbatim("detp_test")
     <<   "Electric field in the active volume: " << detp.Efield() << " kV/cm"
-    << "\nSampling rate:     " << detp.SamplingRate() << " ns"
+    << "\nSampling rate:     " << TDCtick << " ns"
     << "\nArgon temperature: " << detp.Temperature() << " K"
-    << "\nDrift velocity:    " << detp.DriftVelocity() << " cm/us"
-    ;
+    << "\nDrift velocity:    " << driftVelocity << " cm/us";
   
-  // number of ticks
-  unsigned int const nTicks = detp.NumberTimeSamples();
-  constexpr unsigned int nPrintedTicks = 11;
-  mf::LogVerbatim("detp_test")
-    << "Conversion of tick number to x position [cm]"
-    " (detinfo::DetectorProperties::ConvertTicksToX()):";
-  
-  // accumulate the plane IDs
+  // accumulate the plane IDs; needed just for table formatting
   unsigned int const colWidth = 9U;
   unsigned int headerColWidth = 0U;
   for (auto planeID: geom.IteratePlaneIDs()) {
@@ -136,11 +133,62 @@ int main(int argc, char const** argv) {
     if (headerColWidth < l) headerColWidth = l;
   }
   
+  // print drift distances
+  // collect all drift distances, and check whether they are all equal
+  bool allSameDrift = true;
+  lar::util::RealComparisons<double> check(1.0); // 1 cm tolerance
+  auto driftDistances = geom.makeTPCData<double>();
+  for (auto const& TPC: geom.IterateTPCs()) {
+    auto const driftDistance = TPC.DriftDistance();
+    driftDistances[TPC.ID()] = driftDistance;
+    allSameDrift
+      = allSameDrift & check.equal(driftDistance, driftDistances.first());
+  } // for
+  
+  if (allSameDrift) {
+    // print drift distance
+    auto const driftDistance = driftDistances.first();
+    auto const driftTime = driftDistance / driftVelocity;
+    mf::LogVerbatim("detp_test")
+      <<   "Drift distance:    " << driftDistance << " cm"
+      << "\nDrift time:        " << driftTime << " us, "
+                                 << (driftTime / (TDCtick / 1000.)) << " ticks"
+      ;
+  }
+  else {
+    // print drift distance table header
+    mf::LogVerbatim log("detp_test");
+    std::array<unsigned int, 4U> const columnSizes
+      = { headerColWidth, 9U, 13U, 7U };
+    log << std::setw(columnSizes[0]) << "Drift:"
+      << " | " << std::setw(columnSizes[1]) << "time [us]"
+      << " | " << std::setw(columnSizes[2]) << "distance [cm]"
+      << " | " << std::setw(columnSizes[3]) << "ticks";
+    
+    // print drift distances by TPC
+    for (auto const& TPCID: geom.IterateTPCIDs()) {
+      auto const driftDistance = driftDistances[TPCID];
+      auto const driftTime = driftDistance / driftVelocity;
+      log << "\n" << std::setw(columnSizes[0]) << TPCID
+        << " | " << std::setw(columnSizes[1]) << driftTime
+        << " | " << std::setw(columnSizes[2]) << driftDistance
+        << " | " << std::setw(columnSizes[3]) << (driftTime / (TDCtick / 1000.))
+        ;
+    } // for TPC
+  }
+  
+  // print tick number table header
+  // number of ticks
+  unsigned int const nTicks = detp.NumberTimeSamples();
+  constexpr unsigned int nPrintedTicks = 11;
+  mf::LogVerbatim("detp_test")
+    << "Conversion of tick number to x position [cm]"
+    " (detinfo::DetectorProperties::ConvertTicksToX()):";
+  
   std::array<double, nPrintedTicks> ticks;
   for (unsigned int iTick = 0; iTick < nPrintedTicks; ++iTick)
     ticks[iTick] = iTick * nTicks / (nPrintedTicks - 1);
   
-  // print table header
   {
     mf::LogVerbatim log("detp_test");
     log << std::setw(headerColWidth) << "tick #:";
@@ -156,7 +204,7 @@ int main(int argc, char const** argv) {
       log << " | " << std::setw(colWidth) << x;
     } // for tick
   } // for planes
-
+  
   // 4. And finally we cross fingers.
   if (nErrors > 0) {
     mf::LogError("detp_test") << nErrors << " errors detected!";
