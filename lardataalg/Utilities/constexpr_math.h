f1aa9cbc19d88b6a2f32c6b044b795e79f9f7d7a
Gianluca Petrillo <petrillo@slac.stanford.edu>
2019-01-11 12:28:54 -0600

    Added a library with a constexpr version of `abs`.
    This should fix an error in `quantities.h` wirg Clang C++ library.

---
+++ lardataalg/Utilities/constexpr_math.h
@@ -0,0 +1,49 @@
+/**
+ * @file   lardataalg/Utilities/constexpr_math.h
+ * @brief  Mathematical functions that C++ standard doesn't require `constexpr`.
+ * @author Gianluca Petrillo
+ * @date   January 11, 2019
+ *
+ * This is a header-only library.
+ */
+
+#ifndef LARDATAALG_UTILITIES_CONSTEXPR_MATH_H
+#define LARDATAALG_UTILITIES_CONSTEXPR_MATH_H
+
+namespace util {
+  
+  // --- BEGIN simple mathematical functions -----------------------------------
+  /// @name simple mathematical functions
+  /// @{
+  
+  /**
+   * @brief Returns the absolute value of the argument.
+   * @tparam T type of the argument
+   * @param v value to be processed
+   * @return the absolute value of v
+   * 
+   * If the value `v` is negative, its opposite is returned.
+   * Note that this implementation does not work with data types that are not
+   * comparable (like `std::complex`).
+   * 
+   * 
+   * Requirements
+   * -------------
+   * 
+   * * constexpr construction of a `T` value from the literal `0`
+   * * `operator- (T) constexpr`
+   * * `operator< (T, T) constexpr` convertible to `bool`
+   * 
+   */
+  // this bizarre implementation is supposed to guarantee that `abs(-0)` is `0`
+  template <typename T>
+  constexpr auto abs(T v) { return (-T(0) < v)? v: -v; }
+  
+  /// @}
+  
+  // --- END simple mathematical functions -------------------------------------
+  
+} // namespace util
+
+
+#endif // LARDATAALG_UTILITIES_CONSTEXPR_MATH_H
---
+++ test/Utilities/constexpr_math_test.cc
@@ -0,0 +1,46 @@
+/**
+ * @file   test/Utilities/constexpr_math_test.cc
+ * @brief  Unit test for `lardataalg/Utilities/quantities/datasize.h` header.
+ * @author Gianluca Petrillo (petrillo@slac.stanford.edu)
+ * @date   November 2, 2018
+ * @see    `lardataalg/Utilities/quantities.h`
+ *
+ */
+
+// Boost libraries
+#define BOOST_TEST_MODULE ( constexpr_math_test )
+#include <cetlib/quiet_unit_test.hpp> // BOOST_AUTO_TEST_CASE()
+#include <boost/test/test_tools.hpp> // BOOST_CHECK(), BOOST_CHECK_EQUAL()
+
+// LArSoft libraries
+#include "lardataalg/Utilities/constexpr_math.h"
+
+// C/C++ standard libraries
+
+
+// -----------------------------------------------------------------------------
+void test_abs() {
+  
+  static_assert(util::abs(-0.0) == 0.0);
+  static_assert(util::abs(-2.0) == 2.0);
+  static_assert(util::abs(-2.0F) == 2.0F);
+  static_assert(util::abs(-2) == 2);
+  static_assert(util::abs(-2L) == 2L);
+  static_assert(util::abs(-2LL) == 2LL);
+  
+} // test_abs()
+
+
+// -----------------------------------------------------------------------------
+// BEGIN Test cases  -----------------------------------------------------------
+// -----------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(constexpr_math_testcase) {
+  
+  test_abs();
+  
+} // BOOST_AUTO_TEST_CASE(constexpr_math_testcase)
+
+
+// -----------------------------------------------------------------------------
+// END Test cases  -------------------------------------------------------------
+// -----------------------------------------------------------------------------
