24db714ecc0ac1d90d270fb247e94f9427c961ce
Gianluca Petrillo <petrillo@slac.stanford.edu>
2020-01-13 14:32:04 -0600

    Added space quantities based on meter.

--- lardataalg/Utilities/quantities/spacetime.h
+++ lardataalg/Utilities/quantities/spacetime.h
@@ -37,6 +37,11 @@ namespace util::quantities {
       static constexpr auto name   = "second"sv;
     };
 
+    struct Meter: public concepts::UnitBase {
+      static constexpr auto symbol = "m"sv;
+      static constexpr auto name   = "meter"sv;
+    };
+
   } // namespace units
 
 
@@ -353,6 +358,463 @@ namespace util::quantities {
   /// @}
   // -- END Time ---------------------------------------------------------------
 
+
+
+  // -- BEGIN Space ------------------------------------------------------------
+  /**
+   * @name Linear space quantities
+   *
+   * These space quantities are tied to `util::quantities::units::Meter`.
+   * A few options are provided:
+   *
+   * * most general template, `scaled_meter`, allowing to choose both the scale
+   *     of the unit (e.g. `std::milli` for millimeters) and the type of the
+   *     numerical representation
+   * * generic templates (e.g. `meter_as`), allowing to choose which numerical
+   *     representation to use
+   * * double precision (e.g. `meter`), ready for use
+   *
+   * For this unit in particular, additional options are provided to accommodate
+   * the custom of using the unit in plural form: `meters_as` and `meters`
+   * are exactly equivalent to the singular-named counterparts.
+   */
+  /// @{
+
+
+  /// The most generic `units::Meter`-based quantity.
+  template <typename R, typename T = double>
+  using scaled_meter = concepts::scaled_quantity<units::Meter, R, T>;
+
+  //
+  // meters
+  //
+  /// Type of space stored in meters.
+  template <typename T = double>
+  using meter_as = scaled_meter<std::ratio<1>, T>;
+
+  /// Alias for common language habits.
+  template <typename T = double>
+  using meters_as = meter_as<T>;
+
+  /// Type of space stored in meters, in double precision.
+  using meter = meter_as<>;
+
+  /// Alias for common language habits.
+  using meters = meter;
+
+  //
+  // kilometers
+  //
+  /// Type of space stored in kilometers.
+  template <typename T = double>
+  using kilometer_as = concepts::rescale<meter_as<T>, std::kilo>;
+
+  /// Alias for common language habits.
+  template <typename T = double>
+  using kilometers_as = kilometer_as<T>;
+
+  /// Type of space stored in kilometers, in double precision.
+  using kilometer = kilometer_as<>;
+
+  /// Alias for common language habits.
+  using kilometers = kilometer;
+
+  //
+  // centimeters
+  //
+  /// Type of space stored in centimeters.
+  template <typename T = double>
+  using centimeter_as = concepts::rescale<meter_as<T>, std::centi>;
+
+  /// Alias for common language habits.
+  template <typename T = double>
+  using centimeters_as = centimeter_as<T>;
+
+  /// Type of space stored in centimeters, in double precision.
+  using centimeter = centimeter_as<>;
+
+  /// Alias for common language habits.
+  using centimeters = centimeter;
+
+  //
+  // millimeters
+  //
+  /// Type of space stored in millimeters.
+  template <typename T = double>
+  using millimeter_as = concepts::rescale<meter_as<T>, std::milli>;
+
+  /// Alias for common language habits.
+  template <typename T = double>
+  using millimeters_as = millimeter_as<T>;
+
+  /// Type of space stored in millimeters, in double precision.
+  using millimeter = millimeter_as<>;
+
+  /// Alias for common language habits.
+  using millimeters = millimeter;
+
+  //
+  // micrometers
+  //
+  /// Type of space stored in micrometers.
+  template <typename T = double>
+  using micrometer_as = concepts::rescale<meter_as<T>, std::micro>;
+
+  /// Alias for common language habits.
+  template <typename T = double>
+  using micrometers_as = micrometer_as<T>;
+
+  /// Type of space stored in micrometers, in double precision.
+  using micrometer = micrometer_as<>;
+
+  /// Alias for common language habits.
+  using micrometers = micrometer;
+
+  //
+  // nanometers
+  //
+  /// Type of space stored in nanometers.
+  template <typename T = double>
+  using nanometer_as = concepts::rescale<meter_as<T>, std::nano>;
+
+  /// Alias for common language habits.
+  template <typename T = double>
+  using nanometers_as = nanometer_as<T>;
+
+  /// Type of space stored in nanometers, in double precision.
+  using nanometer = nanometer_as<>;
+
+  /// Alias for common language habits.
+  using nanometers = nanometer;
+
+  //
+  // picometers
+  //
+  /// Type of space stored in picometers.
+  template <typename T = double>
+  using picometer_as = concepts::rescale<meter_as<T>, std::pico>;
+
+  /// Alias for common language habits.
+  template <typename T = double>
+  using picometers_as = picometer_as<T>;
+
+  /// Type of space stored in picometers, in double precision.
+  using picometer = picometer_as<>;
+
+  /// Alias for common language habits.
+  using picometers = picometer;
+
+  //
+  // femtometers
+  //
+  /// Type of space stored in femtometers.
+  template <typename T = double>
+  using femtometer_as = concepts::rescale<meter_as<T>, std::femto>;
+
+  /// Alias for common language habits.
+  template <typename T = double>
+  using femtometers_as = femtometer_as<T>;
+
+  /// Type of space stored in femtometers, in double precision.
+  using femtometer = femtometer_as<>;
+
+  /// Alias for common language habits.
+  using femtometers = femtometer;
+
+  /**
+   * @brief Literal constants for space quantities.
+   *
+   * These functions allow a simplified syntax for specifying a space quantity.
+   * In order to use these, their namespace must be used:
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * using namespace util::quantities::space_literals;
+   *
+   * // definition of `util::quantities::meter` constant:
+   * constexpr auto d_m = 12_m;
+   *
+   * [...]
+   *
+   * // assignment (likely to a quantity) of
+   * // `util::quantities::centimeter{500.0}`
+   * d_m = 500_cm;
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   *
+   */
+  namespace space_literals {
+
+    // @{
+    /// Literal meter value.
+    constexpr meter operator""_m (long double v)
+      { return meter{ static_cast<double>(v) }; }
+    constexpr meter operator""_m (unsigned long long int v)
+      { return meter{ static_cast<double>(v) }; }
+    // @}
+
+    // @{
+    /// Literal kilometer value.
+    constexpr kilometer operator""_km (long double v)
+      { return kilometer{ static_cast<double>(v) }; }
+    constexpr kilometer operator""_km (unsigned long long int v)
+      { return kilometer{ static_cast<double>(v) }; }
+    // @}
+
+    // @{
+    /// Literal centimeter value.
+    constexpr centimeter operator""_cm (long double v)
+      { return centimeter{ static_cast<double>(v) }; }
+    constexpr centimeter operator""_cm (unsigned long long int v)
+      { return centimeter{ static_cast<double>(v) }; }
+    // @}
+
+    // @{
+    /// Literal millimeter value.
+    constexpr millimeter operator""_mm (long double v)
+      { return millimeter{ static_cast<double>(v) }; }
+    constexpr millimeter operator""_mm (unsigned long long int v)
+      { return millimeter{ static_cast<double>(v) }; }
+    // @}
+
+    // @{
+    /// Literal micrometer value.
+    constexpr micrometer operator""_um (long double v)
+      { return micrometer{ static_cast<double>(v) }; }
+    constexpr micrometer operator""_um (unsigned long long int v)
+      { return micrometer{ static_cast<double>(v) }; }
+    // @}
+
+    // @{
+    /// Literal nanometer value.
+    constexpr nanometer operator""_nm (long double v)
+      { return nanometer{ static_cast<double>(v) }; }
+    constexpr nanometer operator""_nm (unsigned long long int v)
+      { return nanometer{ static_cast<double>(v) }; }
+    // @}
+
+    // @{
+    /// Literal picometer value.
+    constexpr picometer operator""_pm (long double v)
+      { return picometer{ static_cast<double>(v) }; }
+    constexpr picometer operator""_pm (unsigned long long int v)
+      { return picometer{ static_cast<double>(v) }; }
+    // @}
+
+    // @{
+    /// Literal femtometer value.
+    constexpr femtometer operator""_fm (long double v)
+      { return femtometer{ static_cast<double>(v) }; }
+    constexpr femtometer operator""_fm (unsigned long long int v)
+      { return femtometer{ static_cast<double>(v) }; }
+    // @}
+
+  } // space_literals
+
+
+  // --- BEGIN Space intervals -------------------------------------------------
+
+  namespace intervals {
+
+    /// The most generic `units::Meter`-based interval.
+    template <typename R, typename T = double>
+    using scaled_meters
+      = concepts::Interval<util::quantities::scaled_meter<R, T>>;
+
+    //
+    // meters
+    //
+    /// Type of space interval stored in meters.
+    template <typename T = double>
+    using meters_as = scaled_meters<std::ratio<1>, T>;
+
+    /// Type of space stored in meters, in double precision.
+    using meters = meters_as<>;
+
+    //
+    // kilometers
+    //
+
+    /// Type of space interval stored in kilometers.
+    template <typename T = double>
+    using kilometers_as = scaled_meters<std::kilo, T>;
+
+    /// Type of space interval stored in kilometers, in double precision.
+    using kilometers = kilometers_as<>;
+
+    //
+    // centimeters
+    //
+
+    /// Type of space interval stored in centimeters.
+    template <typename T = double>
+    using centimeters_as = scaled_meters<std::centi, T>;
+
+    /// Type of space interval stored in centimeters, in double precision.
+    using centimeters = centimeters_as<>;
+
+    //
+    // millimeters
+    //
+
+    /// Type of space interval stored in millimeters.
+    template <typename T = double>
+    using millimeters_as = scaled_meters<std::milli, T>;
+
+    /// Type of space interval stored in millimeters, in double precision.
+    using millimeters = millimeters_as<>;
+
+    //
+    // micrometers
+    //
+
+    /// Type of space interval stored in micrometers.
+    template <typename T = double>
+    using micrometers_as = scaled_meters<std::micro, T>;
+
+    /// Type of space interval stored in micrometers, in double precision.
+    using micrometers = micrometers_as<>;
+
+    //
+    // nanometers
+    //
+
+    /// Type of space interval stored in nanometers.
+    template <typename T = double>
+    using nanometers_as = scaled_meters<std::nano, T>;
+
+    /// Type of space interval stored in nanometers, in double precision.
+    using nanometers = nanometers_as<>;
+
+    //
+    // picometers
+    //
+
+    /// Type of space interval stored in picometers.
+    template <typename T = double>
+    using picometers_as = scaled_meters<std::pico, T>;
+
+    /// Type of space interval stored in picometers, in double precision.
+    using picometers = picometers_as<>;
+
+    //
+    // femtometers
+    //
+
+    /// Type of space interval stored in femtometers.
+    template <typename T = double>
+    using femtometers_as = scaled_meters<std::femto, T>;
+
+    /// Type of space interval stored in femtometers, in double precision.
+    using femtometers = femtometers_as<>;
+
+
+  } // namespace intervals
+
+  // --- END Space intervals ---------------------------------------------------
+
+
+  // --- BEGIN Space points ----------------------------------------------------
+
+  namespace points {
+
+    /// The most generic `units::Meter`-based point.
+    template <typename R, typename T = double, typename Cat = NoCategory>
+    using scaled_meter
+      = concepts::Point<util::quantities::scaled_meter<R, T>, Cat>;
+
+    //
+    // meter
+    //
+    /// Type of space point stored in meters.
+    template <typename T = double, typename Cat = NoCategory>
+    using meter_as = scaled_meter<std::ratio<1>, T, Cat>;
+
+    /// Type of space point stored in meters, in double precision.
+    using meter = meter_as<>;
+
+    //
+    // kilometer
+    //
+
+    /// Type of space point stored in kilometers.
+    template <typename T = double, typename Cat = NoCategory>
+    using kilometer_as = scaled_meter<std::kilo, T, Cat>;
+
+    /// Type of space point stored in kilometers, in double precision.
+    using kilometer = kilometer_as<>;
+
+    //
+    // centimeter
+    //
+
+    /// Type of space point stored in centimeters.
+    template <typename T = double, typename Cat = NoCategory>
+    using centimeter_as = scaled_meter<std::centi, T, Cat>;
+
+    /// Type of space point stored in centimeters, in double precision.
+    using centimeter = centimeter_as<>;
+
+    //
+    // millimeter
+    //
+
+    /// Type of space point stored in millimeters.
+    template <typename T = double, typename Cat = NoCategory>
+    using millimeter_as = scaled_meter<std::milli, T, Cat>;
+
+    /// Type of space point stored in millimeters, in double precision.
+    using millimeter = millimeter_as<>;
+
+    //
+    // micrometer
+    //
+
+    /// Type of space point stored in micrometers.
+    template <typename T = double, typename Cat = NoCategory>
+    using micrometer_as = scaled_meter<std::micro, T, Cat>;
+
+    /// Type of space point stored in micrometers, in double precision.
+    using micrometer = micrometer_as<>;
+
+    //
+    // nanometer
+    //
+
+    /// Type of space point stored in nanometers.
+    template <typename T = double, typename Cat = NoCategory>
+    using nanometer_as = scaled_meter<std::nano, T, Cat>;
+
+    /// Type of space point stored in nanometers, in double precision.
+    using nanometer = nanometer_as<>;
+
+    //
+    // picometer
+    //
+
+    /// Type of space point stored in picometers.
+    template <typename T = double, typename Cat = NoCategory>
+    using picometer_as = scaled_meter<std::pico, T, Cat>;
+
+    /// Type of space point stored in picometers, in double precision.
+    using picometer = picometer_as<>;
+
+    //
+    // femtometer
+    //
+
+    /// Type of space point stored in femtometers.
+    template <typename T = double, typename Cat = NoCategory>
+    using femtometer_as = scaled_meter<std::femto, T, Cat>;
+
+    /// Type of space point stored in femtometers, in double precision.
+    using femtometer = femtometer_as<>;
+
+
+  } // namespace points
+
+  // --- END Space points -------------------------------------------------------
+
+
+  /// @}
+  // -- END Space --------------------------------------------------------------
 
 
 
---
+++ test/Utilities/space_test.cc
@@ -0,0 +1,115 @@
+/**
+ * @file   test/Utilities/space_test.cc
+ * @brief  Unit test for `lardataalg/Utilities/quantities/spacetime.h` header.
+ * @author Gianluca Petrillo (petrillo@slac.stanford.edu)
+ * @date   January 13, 2020
+ * @see    `lardataalg/Utilities/quantities.h`
+ *
+ * This test covers only the space units of `spacetime.h`.
+ * It's not overwhelmingly complete, either.
+ */
+
+// Boost libraries
+#define BOOST_TEST_MODULE ( space_test )
+#include <cetlib/quiet_unit_test.hpp> // BOOST_AUTO_TEST_CASE()
+#include <boost/test/test_tools.hpp> // BOOST_CHECK(), BOOST_CHECK_EQUAL()
+
+// LArSoft libraries
+#include "lardataalg/Utilities/quantities/spacetime.h"
+
+// C/C++ standard libraries
+#include <iostream>
+#include <type_traits> // std::decay_t<>
+
+
+// -----------------------------------------------------------------------------
+void test_meter_literals() {
+
+  using namespace util::quantities::space_literals;
+  
+  //
+  // NOTE: the test with physics constants is cool, but floating point
+  //       arithmetics having finite precision, static tests based on equality
+  //       may fail... if that happens, my magic recipe is... use less digits.
+  //       Wow.
+  //
+
+  constexpr auto l_m = 299792458_m; // space covered by a photon in vacuum in 1"
+  static_assert
+    (std::is_same<decltype(l_m), util::quantities::meter const>());
+  static_assert(l_m.value() == 299792458);
+  static_assert(l_m == 299792458_m);
+  std::cout << "Tested " << l_m << std::endl;
+
+  constexpr auto l_km = 299792.458_km;
+  static_assert
+    (std::is_same<decltype(l_km), util::quantities::kilometer const>());
+  static_assert(l_km == 299792.458_km);
+  static_assert(l_km.value() == 299792.458);
+  static_assert(l_km == 299792458_m);
+  std::cout << "Tested " << l_km << std::endl;
+
+  constexpr auto l_cm = 29979245800_cm;
+  static_assert(std::is_same<decltype
+    (l_cm), util::quantities::centimeter const>());
+  static_assert(l_cm == 29979245800_cm);
+  static_assert(l_cm.value() == 29979245800);
+  static_assert(l_cm == 299792458_m);
+  std::cout << "Tested " << l_cm << std::endl;
+
+  constexpr auto l_mm = 299792458000_mm;
+  static_assert
+    (std::is_same<decltype(l_mm), util::quantities::millimeter const>());
+  static_assert(l_mm == 299792458000_mm);
+  static_assert(l_mm.value() == 299792458000);
+  static_assert(l_mm == 299792458_m);
+  std::cout << "Tested " << l_mm << std::endl;
+
+  constexpr auto l_um = 1063_um; // dE/dl peak of CMB according to Wikipedia
+  static_assert
+    (std::is_same<decltype(l_um), util::quantities::micrometer const>());
+  static_assert(l_um == 1063_um);
+  static_assert(l_um.value() == 1063);
+  static_assert(l_um == 1.063_mm);
+  std::cout << "Tested " << l_um << std::endl;
+
+  constexpr auto l_nm = 1063000_nm;
+  static_assert
+    (std::is_same<decltype(l_nm), util::quantities::nanometer const>());
+  static_assert(l_nm == 1063000_nm);
+  static_assert(l_nm.value() == 1063000);
+  static_assert(l_nm == 1063_um);
+  std::cout << "Tested " << l_nm << std::endl;
+
+  constexpr auto l_pm = 1063000000_pm;
+  static_assert
+    (std::is_same<decltype(l_pm), util::quantities::picometer const>());
+  static_assert(l_pm == 1063000000_pm);
+  static_assert(l_pm.value() == 1063000000);
+  static_assert(l_pm == 1063000_nm);
+  std::cout << "Tested " << l_pm << std::endl;
+
+  constexpr auto l_fm = 52918_fm; // Bohr radius (rounded)
+  static_assert
+    (std::is_same<decltype(l_fm), util::quantities::femtometer const>());
+  static_assert(l_fm == 52918_fm);
+  static_assert(l_fm.value() == 52918);
+  static_assert(l_fm == 52.918_pm);
+  std::cout << "Tested " << l_fm << std::endl;
+
+} // test_meter_literals()
+
+
+// -----------------------------------------------------------------------------
+// BEGIN Test cases  -----------------------------------------------------------
+// -----------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(meter_testcase) {
+
+  test_meter_literals();
+
+} // BOOST_AUTO_TEST_CASE(meter_testcase)
+
+
+// -----------------------------------------------------------------------------
+// END Test cases  -------------------------------------------------------------
+// -----------------------------------------------------------------------------
